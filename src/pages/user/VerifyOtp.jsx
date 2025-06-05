21fa557b380cbbdab8f87174ea1eb66dddc58412
Bhargav Kola <bhargavkola53@gmail.com>
2025-06-05 15:09:06 +0530

    Forget-Password

---
+++ src/pages/user/VerifyOtp.jsx
@@ -0,0 +1,38 @@
+// src/pages/user/VerifyOtp.js
+import React, { useState } from "react";
+import { verifyOTP } from "../../services/authService";
+
+const VerifyOTP = ({ emailOrPhone, onSuccess, onBack }) => {
+  const [otp, setOtp] = useState("");
+  const [error, setError] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await verifyOTP(emailOrPhone, otp);
+      onSuccess(); // Move to reset password
+    } catch (err) {
+      setError(err.response?.data?.message || "Invalid OTP.");
+    }
+  };
+
+  return (
+    <div className="form-container"> 
+    <form onSubmit={handleSubmit}>
+      <h3>Enter OTP sent to {emailOrPhone}</h3>
+      <input
+        type="text"
+        value={otp}
+        onChange={(e) => setOtp(e.target.value)}
+        placeholder="Enter OTP"
+        required
+      />
+      <button type="submit">Verify OTP</button>
+      <button type="button" onClick={onBack}>Back</button>
+      {error && <p style={{ color: "red" }}>{error}</p>}
+    </form>
+    </div>
+  );
+};
+
+export default VerifyOTP;
